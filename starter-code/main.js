19cb12f2f5cf752d4ce42c2e4afaccd8afcd52a4
Lee Yi Sheng <yisheng@yishenglee.com>
2016-11-16 13:08:05 +0800

    create javascript to create cards

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,8 +5,20 @@ var cardTwo = "Queen";
 var cardThree = "King";
 var cardFour = "King";
 
-if (cardOne === cardTwo) {
-	alert('You found a match!');
-} else {
-	alert('Sorry, try again.');
+//if (cardOne === cardTwo) {
+//	alert('You found a match!');
+//} else {
+//	alert('Sorry, try again.');
+//}
+
+var gameBoard = document.getElementById('game-board');
+
+function createCards() {
+	for (var a = 1; a <= 4; a++) {
+		 var card = document.createElement('div');
+	 		card.className = 'cards';
+	 		gameBoard.appendChild(card);
+	}
 }
+
+gameBoard.addEventListener('load', createCards());
